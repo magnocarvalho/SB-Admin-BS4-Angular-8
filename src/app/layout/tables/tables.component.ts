0075266ec057520ddcf7689bae8fbab112f5300d
magno carvalho <magnocarv@hotmail.com>
2020-06-26 10:05:06 -0300

    tabela corrigida

--- src/app/layout/tables/tables.component.ts
+++ src/app/layout/tables/tables.component.ts
@@ -4,33 +4,50 @@ import { FbApiService } from '../../api/fb-api.service';
 import { ExcelService } from '../../api/excel.service';
 
 @Component({
-  selector: 'app-tables',
-  templateUrl: './tables.component.html',
-  styleUrls: ['./tables.component.scss'],
-  animations: [routerTransition()]
+    selector: 'app-tables',
+    templateUrl: './tables.component.html',
+    styleUrls: ['./tables.component.scss'],
+    animations: [routerTransition()]
 })
 export class TablesComponent implements OnInit {
-  public clientes: Array<any> = [];
-  public export_clientes: Array<any> = [];
+    public clientes: Array<any> = [];
+    public export_clientes: Array<any> = [];
 
-  constructor(private api: FbApiService, private excel: ExcelService) {}
+    constructor(private api: FbApiService, private excel: ExcelService) {}
 
-  ngOnInit() {
-    this.api.getClientesAlex().subscribe((data) => {
-      this.export_clientes = data.map((e) => {
-        return e.payload.doc.data();
-      });
-      this.clientes = data.map((e) => {
-        return { id: e.payload.doc.id, cliente: e.payload.doc.data() } as any;
-      });
-    });
-  }
-  generaterExcel() {
-    this.export_clientes.forEach((e) => {
-      e.dataReivindicacao = new Date(e.dataReivindicacao.toDate());
-      return e;
-    });
-    debugger;
-    this.excel.exportAsExcelFile(this.export_clientes, 'clientes_alex');
-  }
+    ngOnInit() {
+        this.api.getClientesAlex().subscribe((data) => {
+            this.export_clientes = data.map((e) => {
+                return e.payload.doc.data();
+            });
+            this.clientes = data.map((e) => {
+                return { id: e.payload.doc.id, cliente: e.payload.doc.data() } as any;
+            });
+        });
+    }
+    generaterExcel() {
+        this.export_clientes.forEach((e) => {
+            e.dataReivindicacao = new Date(e.dataReivindicacao.toDate());
+            e.tipo = this.tipoDeContato(e.tipo)
+            return e;
+        });
+        debugger;
+        this.excel.exportAsExcelFile(this.export_clientes, 'clientes_alex');
+    }
+    tipoDeContato(t) {
+        switch (t) {
+            case 1:
+                return 'Reclamação';
+            case 2:
+                return 'Elogio';
+            case 3:
+                return 'Sugestão';
+            case 4:
+                return 'Denúncia';
+            case 5:
+                return 'Contato';
+            case 6:
+                return 'Solicitação';
+        }
+    }
 }
